d3b4acbe1aa211e9922fd39659e729de78e92cc9
Esteban198 <esteban.glz198@gmail.com>
2025-12-16 12:30:20 -0800

    Add project files.

---
+++ Maroodi.Application/UseCases/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Maroodi.Application.Interfaces;
+using Maroodi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maroodi.Application.UseCases
+{
+    public class UserService
+    {
+
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository) {
+        _userRepository = userRepository;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _userRepository.GetAllUsers();
+        }
+
+        public User GetUserById(int id)
+        {
+            return _userRepository.GetById(id);
+        }
+    }
+}
