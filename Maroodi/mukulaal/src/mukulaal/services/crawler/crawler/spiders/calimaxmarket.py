3f6fe1f8d8da894b30f34f1c3502f062304f95f9
Daniel Ramos <daniel.ramos.rivera.dev@gmail.com>
2026-01-31 18:02:30 -0800

    Rfactor parse and paritems functions

--- Maroodi/mukulaal/src/mukulaal/services/crawler/crawler/spiders/calimaxmarket.py
+++ Maroodi/mukulaal/src/mukulaal/services/crawler/crawler/spiders/calimaxmarket.py
@@ -42,33 +42,36 @@ class CalimaxmarketSpider(scrapy.Spider):
             product_item.add_css("amount","span.vtex-product-summary-2-x-productBrand::text")
             product_item.add_css("unit", "span.vtex-product-summary-2-x-productBrand::text")
 
-            price_label = i.css("span.vtex-product-price-1-x-sellingPrice--summary")
-            price_int_label = price_label.css("span.vtex-product-price-1-x-currencyInteger--summary::text").get()
-            price_frac_label = price_label.css("span.vtex-product-price-1-x-currencyFraction--summary::text").get()
+            price_int_label = i.css("span.vtex-product-price-1-x-currencyInteger--summary::text").get()
+            price_frac_label = i.css("span.vtex-product-price-1-x-currencyFraction--summary::text").get()
+
             price_value = int(price_int_label) + int(price_frac_label) / 100
             product_item.add_value("price", price_value)
             product_item.add_value("section", response.url.split("/")[-1])
             yield product_item.load_item()
 
     def parse(self, response):
-        categories = response.css("ul")[3].css("a")[2:]
-        for item in categories[:3]:
-            self.categories_queue.append(item.xpath("@href").get().split("/")[-1])
+        try:
+            categories = response.css("ul")[3].css("a")[2:]
+            for item in categories:
+                self.categories_queue.append(item.xpath("@href").get().split("/")[-1])
 
-        print("="*60)
-        print("     Extracting Categories")
-        while len(self.categories_queue) > 0:
-            curr_category = self.categories_queue.popleft()
-            print(f"        Category {curr_category}")
-            yield scrapy.Request(url=f"https://tienda.calimax.com.mx/{curr_category}",
-                                 callback=self.parse_items,
-                                 meta={
-                                     "playwright": True,
-                                     "playwright_include_page": True,
-                                     "load_site": True,
-                                     "playwright_page":response.meta["playwright_page"],
-                                     "playwright_page_methods": [
-                                         PageMethod("wait_for_selector", "section.vtex-product-summary-2-x-container")
-                                     ],
-                                 }
-                                )
+            print("="*60)
+            print("     Extracting Categories")
+            while len(self.categories_queue) > 0:
+                curr_category = self.categories_queue.popleft()
+                print(f"        Category {curr_category}")
+                yield scrapy.Request(url=f"https://tienda.calimax.com.mx/{curr_category}",
+                                     callback=self.parse_items,
+                                     meta={
+                                         "playwright": True,
+                                         "playwright_include_page": True,
+                                         "load_site": True,
+                                         "playwright_page":response.meta["playwright_page"],
+                                         "playwright_page_methods": [
+                                             PageMethod("wait_for_selector", "section.vtex-product-summary-2-x-container")
+                                         ],
+                                     }
+                                    )
+        except Exception as e:
+            print(f"Error Parsing {curr_category}: {e}")
