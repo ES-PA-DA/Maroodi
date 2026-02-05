3760f44f10c150c5198b587ca7f46a2f9499da4b
LEPT <lept.dev@gmail.com>
2026-02-04 20:36:03 -0800

    CustomSelect Component

--- baaz/app/(tabs)/index.tsx
+++ baaz/app/(tabs)/index.tsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import Header from "@components/Header";
 import BaseScreen from "@components/BaseScreen";
+import CustomSelect from "@components/CustomSelect";
+
+const stores = [
+  {
+    id: 1,
+    name: "Calimax"
+  },
+  {
+    id: 2,
+    name: "Ley"
+  },
+  {
+    id: 3,
+    name: "Walmart"
+  },
+];
 
 export default function Index() {
+
+  const [selectedStore, setSelectedStore] = useState("");
+
   return (
     <BaseScreen>
       <Header>New Price</Header>
+      <CustomSelect
+        placeholder="Select an option"
+        items={ stores }
+        selectedItem={selectedStore}
+        setSelectedItem={setSelectedStore}
+      />
     </BaseScreen>
   );
 }
