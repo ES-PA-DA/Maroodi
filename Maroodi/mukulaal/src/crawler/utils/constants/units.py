ce84464494160e70f1cdfbbe74ec9dfcae153ae9
Daniel Ramos <daniel.ramos.rivera.dev@gmail.com>
2026-04-10 11:56:30 -0700

    MRI-15: Remove C# API folders and files

--- Maroodi/mukulaal/src/crawler/utils/constants/units.py
+++
@@ -1,2 +0,0 @@
-KG = "KG"
-PZA = "PZA"
--- Maroodi/mukulaal/src/crawler/utils/utils.py
+++
@@ -1,5 +0,0 @@
-def str_to_list(list_str:str):
-    try:
-        return list_str.split(",")
-    except Exception as err:
-        print("Error parsing str list to list: ", err)
