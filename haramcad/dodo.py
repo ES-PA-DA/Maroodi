ff6dfdd507ebd97d4912853703130b0b4483cddb
Daniel Ramos <daniel.ramos.rivera.dev@gmail.com>
2026-04-10 12:13:18 -0700

    MRI-15: Format dodo.py

--- haramcad/dodo.py
+++ haramcad/dodo.py
@@ -1,10 +1,13 @@
 from doit import task_params
 
 
-@task_params(
-    [{"name": "name", "default": "auth", "long": "name", "short": "n"}]
-)
+@task_params([{"name": "name", "default": "auth", "long": "name", "short": "n"}])
 def task_create_service(name):
     """Create basic files for every new service"""
-    base_path = f"./src/{name}" 
-    return {"actions": [f"mkdir {base_path}", f"touch {base_path}/__init__.py {base_path}/router.py {base_path}/models.py {base_path}/schemas.py"]}
+    base_path = f"./src/{name}"
+    return {
+        "actions": [
+            f"mkdir {base_path}",
+            f"touch {base_path}/__init__.py {base_path}/router.py {base_path}/models.py {base_path}/schemas.py",
+        ]
+    }
